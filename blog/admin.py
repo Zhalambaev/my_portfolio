45548f7f1f26e13f505138d6729d399be42554d5
Denis Zhalambaev <cnchelper@yandex.ru>
2024-01-03 14:58:49 +0700

    Сайт закончен, выглядит не очень

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Blog_project
 
-# Register your models here.
+admin.site.register(Blog_project)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog_project(models.Model):
+
+    title_blog = models.CharField(max_length=200)
+    date_blog = models.DateTimeField(auto_now=True)
+    description_blog = models.TextField()
+    url_blog = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title_blog
+
